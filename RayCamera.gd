e1d824b9d5b4169cdd4372b58186ec86c5d2a84a
szczyprian <cyprian.a@vp.pl>
2023-12-17 16:34:18 +0100

    Added level time and level time display

--- RayCamera.gd
+++ RayCamera.gd
@@ -1,6 +1,8 @@
 extends Camera3D
 
 @export var max_bullets:int = 3
+@export var game_time:float = 30.0
+
 var points:int:
 	set(points_in):
 		points= points_in
@@ -11,9 +13,18 @@ var bullets:int:
 		bullets = bullets_in
 		label.text = "Bullets: " + str(bullets)
 
+var time:float:
+	set(time_in):
+		time = time_in
+		if time >=0.0:
+			time_label.text = "Time: " + str(round(time))
+		else:
+			time_label.text = "Level Ended"
+
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 @onready var label: Label = $MarginContainer/Label
 @onready var points_label: Label = $MarginContainer/PointsLabel
+@onready var time_label: Label = $MarginContainer/TimeLabel
 
 
 
@@ -22,9 +33,11 @@ func  _ready() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_CROSS)
 	bullets = max_bullets
 	points =0;
+	time= game_time
 	
 	
 func _process(delta: float) -> void:
+	time -= delta
 	var mouse_positon:Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_positon) * 100.0
 	ray_cast_3d.force_raycast_update()
--- TargetSpawner.gd
+++ TargetSpawner.gd
@@ -4,14 +4,24 @@ extends Area3D
 @export var target_speed:float = 3.0
 @export var target_direction:Vector3 = Vector3.RIGHT
 @export var delete_delay:float = 2.5
+@export var spawning_time:float = 30.0
+
+@onready var timer: Timer = $Timer
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var tween = create_tween()
+	tween.tween_interval(spawning_time)
+	tween.tween_callback(timer.stop)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if spawning_time >=0.0:
+		spawning_time-=delta
+		print(spawning_time)
 
 func spawn_targert() ->void:
 	var new_target = target.instantiate()
